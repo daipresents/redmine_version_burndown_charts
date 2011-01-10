7af40054b001bccf65dd687d81b42d7503d58780
Yoichi Izumi <quanyangyi0323@gmail.com>
2011-01-10 10:05:33 +0900

    add RSpec

---
+++ spec/redmine_version_burndown_charts_spec.rb
@@ -0,0 +1,6 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "redmine_version_burndown_charts" do  
+  it "should have a pending spec"  
+end  
+
---
+++ tasks/redmine_version_burndown_charts_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :redmine_version_burndown_charts do
+#   # Task goes here
+# end
